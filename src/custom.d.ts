a95541d4ee31adca4248c37dadafaf3a5c351e5c
vikas07 <vickykap1992@gmail.com>
2023-04-26 15:14:07 +0530

    Add files via upload

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+//Global declartion
+declare module '*.svg'{
+    import React =  require("react");
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
